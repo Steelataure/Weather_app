430e35de49f16add5a05b6db48fa026473867c2b
Steel <alexandre0312@orange.fr>
2022-06-19 20:01:59 +0200

    navbar

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,12 @@
 import '../styles/Banner.css'
+import logo from '../assets/navbar.png'
 
-function    Banner(){
+function Banner(){
+    const title  = "Méteo"
     return (
     <div className="MyBanner">
-        <h1>Application méteo</h1>
+         <img src={logo} alt='appli méteo' className='weather-logo' />
     </div>
 )}
-
-
 
 export default Banner
